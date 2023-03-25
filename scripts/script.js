f82e745622e3c993d75e165229dd2187bc67ea9e
Kayke Bauer <bauer.kaykemarins@gmail.com>
2023-03-25 13:54:54 -0300

    merge

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-function movieDetail($id){
-  if ($id<5) {
+function movieDetail($id) {
+  if ($id < 5) {
     document.querySelector('#movieDetail5').classList.add('show');
-    document.querySelector('#movieDetail10').classList.remove('show');    
-  }else{
+    document.querySelector('#movieDetail10').classList.remove('show');
+  } else {
     document.querySelector('#movieDetail10').classList.add('show');
     document.querySelector('#movieDetail5').classList.remove('show');
   }
@@ -10,7 +10,7 @@ function movieDetail($id){
 function closeMovieDetail() {
   const elements = document.querySelectorAll('.movie-details');
 
-elements.forEach((element) => {
-  element.classList.remove('show');
-});
+  elements.forEach((element) => {
+    element.classList.remove('show');
+  });
 }
