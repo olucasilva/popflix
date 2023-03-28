04d49cd54710dcc09dc729c7f3c46b453042a22d
Kayke Bauer <bauer.kaykemarins@gmail.com>
2023-03-27 22:52:09 -0300

    Add shopping-cart in the header

--- components/header.php
+++ components/header.php
@@ -3,10 +3,17 @@
         <h1>PobreFlix</h1>
     </div>
     <nav>
-        <ul>
-            <li> <a href="">Home</a></li>
-            <li> <a href="">Most View</a></li>
-            <li> <a href="">Contact</a></li>
-        </ul>
+        <div>
+            <ul>
+                <li> <a class="navLink" href="">Home</a></li>
+                <li> <a class="navLink" href="">Most View</a></li>
+                <li> <a class="navLink" href="">Contact</a></li>
+            </ul>
+        </div>    
     </nav>
+    <div>
+        <a href="">
+            <img src="../img/shopping-cart (1).png">  
+        </a>
+    </div>
 </header>
