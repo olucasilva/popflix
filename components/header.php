59dfc74c571b79222fbf926b86c6aaeadaca433c
Kayke Bauer <bauer.kaykemarins@gmail.com>
2023-03-28 21:38:04 -0300

    Re-styling the index_cadastro

--- components/header.php
+++ components/header.php
@@ -5,14 +5,14 @@
     <nav>
         <div>
             <ul>
-                <li> <a class="navLink" href="">Home</a></li>
+                <li> <a class="navLink" href="../index.php">Home</a></li>
                 <li> <a class="navLink" href="">Most View</a></li>
                 <li> <a class="navLink" href="">Contact</a></li>
             </ul>
         </div>    
     </nav>
     <div>
-        <a href="">
+        <a href="../index_cadastro.html">
             <img src="../img/shopping-cart (1).png">  
         </a>
     </div>
