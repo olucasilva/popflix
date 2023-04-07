53d9800c6f947d7d6746f743f1092800695844f7
Kayke Bauer <bauer.kaykemarins@gmail.com>
2023-04-07 11:48:03 -0300

    NotaFiscal

--- server/rent.php
+++ server/rent.php
@@ -3,19 +3,23 @@ date_default_timezone_set('America/Sao_Paulo');
 $dataFilmes = file_get_contents('../data/movies.json');
 $itemsFilmes = json_decode($dataFilmes);
 
-$dataSeries = simplexml_load_file('../data/series.xml')->series->serie;
+$dataSeries = simplexml_load_file('../data/series.xml')->series;
 
-$total = 0;
 
-function gravar($email, $name, $titles, $price, $totalF, $cidade, $id, $idsSeries, $nameS, $priceS, $totalS, $totalT)
+function gravar($email, $personName, $titles, $totalF, $id, $idsSeries, $titlesSeries, $totalS, $totalT)
 {
+  $url = "http://ip-api.com/json";
+  $response = file_get_contents($url);
+  $data = json_decode($response, true);
+  $cidade = isset($data['city']) ? $data['city'] : 'Não identificado';
+
   $totalT = "R$" . number_format($totalT, 2, ',', '.');
   $totalS = "R$" . number_format($totalS, 2, ',', '.');
   $totalF = "R$" . number_format($totalF, 2, ',', '.');
   $timestamp = date("d-m-Y_H_i_s");
   $arquivo = "./notaFiscal_$timestamp.txt";
   $fp = fopen($arquivo, 'a+');
-  fwrite($fp, "Nome: " . $name);
+  fwrite($fp, "Nome: " . $personName);
   fwrite($fp, "\n");
   fwrite($fp, "Email: " . $email);
   fwrite($fp, "\n");
@@ -23,17 +27,23 @@ function gravar($email, $name, $titles, $price, $totalF, $cidade, $id, $idsSerie
   fwrite($fp, "\n");
   fwrite($fp, "Data: " . date("d-m-Y H:i:s"));
   fwrite($fp, "\n");
+  fwrite($fp, "------------------------------------");
+  fwrite($fp, "\n");
   fwrite($fp, "Filmes alugados: " . $titles);
   fwrite($fp, "\n");
   fwrite($fp, "ID dos filmes alugados: " . $id);
   fwrite($fp, "\n");
   fwrite($fp, "Valor pago nos filmes: " . $totalF);
   fwrite($fp, "\n");
-  fwrite($fp, "Series alugados: " . $nameS);
+  fwrite($fp, "------------------------------------");
+  fwrite($fp, "\n");
+  fwrite($fp, "Series alugados: " . $titlesSeries);
   fwrite($fp, "\n");
   fwrite($fp, "ID das series alugadas: " . $idsSeries);
   fwrite($fp, "\n");
   fwrite($fp, "Valor pago nas series: " . $totalS);
+  fwrite($fp, "\n");
+  fwrite($fp, "------------------------------------");
   fwrite($fp, "\n");
   fwrite($fp, "Valor pago: " . $totalT);
   fclose($fp);
@@ -44,45 +54,39 @@ function getItems($itemsFilmes, $dataSeries)
 {
   if (isset($_POST['firstname'], $_POST['email'])) {
     $email = $_POST['email'];
-    $name = $_POST['firstname'];
+    $personName = $_POST['firstname'];
   }
-
-  $ids = "";
+  $totalF = 0;
+  $id = "";
   $titles = "";
   foreach ($itemsFilmes as $item) {
-    foreach ($_COOKIE as $key => $ids) {
+    foreach ($_COOKIE as $key => $n) {
       if ($item->id == $key) {
-        $id = $id . "\n" . $item->id;
-        $titles = $titles . "\n" . $item->title;
-        $price = "R$" . number_format($item->price, 2, ',', '.');
+        $id = $id . "\n\t - " . $item->id;
+        $titles = $titles . "\n\t - " . $item->title;
         $totalF += $item->price;
       }
     }
   }
 
-
-  $nameS = "";
+  $totalS = 0;
+  $titlesSeries = "";
   $idsSeries = "";
 
-  foreach ($dataSeries as $data) {
+  foreach ($dataSeries->children() as $data) {
     foreach ($_COOKIE as $key => $ids) {
       if ($data->id == $key) {
-        $idsSeries = $idsSeries . "\n" . $data->id;
-        $nameS = $nameS . "\n" . $dataSeries->$name;
-        $priceS = floatval($data->price);
-        $priceS = "R$" . number_format($priceS, 2, ',', '.');
-        $totalS += $data->price;
+        $idsSeries = $idsSeries . "\n\t - " . $data->id;
+        $titlesSeries = $titlesSeries . "\n\t - " . $data->name;
+        $totalS += floatval($data->price);
       }
     }
   }
+  $totalT = 0;
   $totalT = $totalF + $totalS;
 
-  $url = "http://ip-api.com/json";
-  $response = file_get_contents($url);
-  $data = json_decode($response, true);
-  $cidade = isset($data['city']) ? $data['city'] : 'Não identificado';
 
-  gravar($email, $name, $titles, $price, $totalF, $cidade, $id, $idsSeries, $nameS, $priceS, $totalS, $totalT);
+  gravar($email, $personName, $titles, $totalF, $id, $idsSeries, $titlesSeries, $totalS, $totalT);
 }
 getItems($itemsFilmes, $dataSeries);
 
